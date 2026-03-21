1a7de1b884501cd67ff159abadc50ee5763fd2f8
S <S>
2026-03-21 10:35:45 +0300

    Update dependency versions. Add new dependency to FCM. Create core:core-fcm module. Optimize Yandex MapKitSdk usage(remove potentially MemoryLeak). In EnterpriseScreen and EnterprisesViewModel.kt add check: is MapKitController ready or not. Also add CompletableDeferred mechanism. In DrawMap function on Android-Side, add logic to request permissions and initialize MapKitFactory. Add FcmService to receive push-notifications and add DeepLinks mechanism with android.net.Uri to add navigation to ProductDetailsScreen from notification. Optimize DI mechanism using stopKoin function before initialize new KoinInstant. Remove initialization MapKitFactory in MainActivity onStart(). Update logic to verify authenticated user to AppViewModel.kt. In Android-Side to  this, update SplashScreenAPI. Turn off auto backup in AndroidManifest.xml.

--- androidApp/src/main/java/com/foodsaver/app/androidApp/MainActivity.kt
+++ androidApp/src/main/java/com/foodsaver/app/androidApp/MainActivity.kt
@@ -3,8 +3,6 @@ package com.foodsaver.app.androidApp
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
